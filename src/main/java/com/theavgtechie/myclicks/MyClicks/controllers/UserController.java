6c0dded0b3dd30f3c57c556630d8fea69565b756
Sritaj <sritajp@gmail.com>
2020-12-27 19:54:09 +0530

    Added sample templates for create account, forgot password and welcome page, and added relevant controllers for the same

--- src/main/java/com/theavgtechie/myclicks/MyClicks/controllers/UserController.java
+++ src/main/java/com/theavgtechie/myclicks/MyClicks/controllers/UserController.java
@@ -1,9 +1,39 @@
 package com.theavgtechie.myclicks.MyClicks.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
 
-    
+    // User Registration Methods
+    @RequestMapping("/registration")
+    public String userRegistration(Model model){
+
+        return "create-account";
+    }
+
+    @RequestMapping("/cancelRegistration")
+    public String cancelRegistration(){
+
+        return "redirect:/";
+    }
+
+    // Forgot Password Methods
+    @RequestMapping("/forgotPassword")
+    public String forgotPassword(Model model){
+
+        return "forgot-password";
+    }
+
+    @RequestMapping("/cancelForgotPassword")
+    public String cancelfForgotPassword(Model model){
+
+        return "redirect:/";
+    }
+
+
+
+
 }
